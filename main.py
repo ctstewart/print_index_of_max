fe7254667dd8053cff95579df075f921e70a3dc1
heelercs <heelercs@users.noreply.github.com>
2022-01-18 09:22:50 -0600

    The script now prompts for user input

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def print_index_of_max(a_list):
 	max_index = 0
 	max_num = a_list[0]
 	for index, i in enumerate(a_list):
+		i = int(i)
 		if (i > max_num):
 			max_index = index
 			max_num = i
 	return max_index
 
 if __name__ == "__main__":
-	my_list = [1, 5, 7, 3, 10, 45, 7, -5]
+	my_int = split(input("Please enter a list of integers separated by spaces"))
 	print(print_index_of_max(my_list))
