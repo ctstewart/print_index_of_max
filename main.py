65e674675bb0139f7902f6df605bcc2001bffc9a
heelercs <heelercs@users.noreply.github.com>
2022-01-18 09:26:15 -0600

    Fixed changing the list of str to a list of int

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def print_index_of_max(a_list):
+	a_list = [int(i) for i in a_list]
 	max_index = 0
 	max_num = a_list[0]
 	for index, i in enumerate(a_list):
-		i = int(i)
 		if (i > max_num):
 			max_index = index
 			max_num = i
