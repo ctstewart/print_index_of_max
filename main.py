2469d8337fc96d1d1b237492e311572ccb009e69
heelercs <heelercs@users.noreply.github.com>
2022-01-18 09:16:58 -0600

    Created main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def print_index_of_max(a_list):
+	max_index = 0
+	max_num = a_list[0]
+	for index, i in enumerate(a_list):
+		print(index)
+		if (i > max_num):
+			max_index = index
+			max_num = i
+	return max_index
+
+if __name__ == "__main__":
+  my_list = [1, 5, 7, 3, 10, 45, 7, -5]
+  print(print_index_of_max(my_list))
